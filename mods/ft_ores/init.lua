586b7e2bb2848c92bc5048b18ec7bc543a92b344
TehnoTheDragon <tehnothedragon@gmail.com>
2023-12-23 22:46:00 +0200

    useless ores

---
+++ mods/ft_ores/init.lua
@@ -0,0 +1,12 @@
+local ore = ft_ore.ore
+
+ore("coal", {
+    visual = {
+        main = "stone.png",
+        overlay = "ore_template_type_2.png",
+        tint = "#1d1d1d"
+    },
+    tags = {
+        soft = 2
+    }
+})
